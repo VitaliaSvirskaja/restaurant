f0b9f974e890308934ffdb7501dd2128eb51f386
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-04-11 08:57:07 +0200

    Added switch Tab function

--- src/homepageRestaurant.ts
+++ src/homepageRestaurant.ts
@@ -1,22 +1,26 @@
-// <p>Best traditional sushi in your country.</p>
-// <p>Every single sushi roll is prepared by our Japanese chef in the traditional way with
-//     a lot of precision and love.</p>
-// <p>Founded and managed by the Shisui family since 1920.</p>
-// <div class="opening-hours">
-//     <h3>Opening hours</h3>
-// <p>Monday: 11am - 10pm</p>
-// <p>Tuesday: 11am - 10pm</p>
-// <p>Wednesday: 11am - 10pm</p>
-// <p>Thursday: 11am - 10pm</p>
-// <p>Friday: 11am - 10pm</p>
-// <p>Saturday: 11am - 10pm</p>
-// <p>Sunday: 11am - 10pm</p>
-// </div>
-
 const contentContainer = document.querySelector("#content");
+const templateHome = document.getElementById(
+  "homeContent"
+) as HTMLTemplateElement;
+const templateMenu = document.getElementById(
+  "menuContent"
+) as HTMLTemplateElement;
+
+const templateContact = document.getElementById(
+  "contactContent"
+) as HTMLTemplateElement;
 
 export function addHomeContent() {
-  const firstText = document.createElement("p");
-  firstText.innerHTML = "Best traditional sushi in your country.";
-  contentContainer?.appendChild(firstText);
+  const contentHome = templateHome!.content.cloneNode(true);
+  contentContainer?.appendChild(contentHome);
+}
+
+export function addMenuContent() {
+  const contentMenu = templateMenu!.content.cloneNode(true);
+  contentContainer?.appendChild(contentMenu);
+}
+
+export function addContactContent() {
+  const contentContact = templateContact!.content.cloneNode(true);
+  contentContainer?.appendChild(contentContact);
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,31 @@
 import "./style.css";
-import { addHomeContent } from "./homepageRestaurant";
+import {
+  addContactContent,
+  addHomeContent,
+  addMenuContent,
+} from "./homepageRestaurant";
+
+const contentContainer = document.querySelector("#content");
+
+function initPage() {
+  addHomeContent();
+}
+initPage();
 
 const homeButton = document.querySelector(".homeBtn");
 homeButton?.addEventListener("click", () => {
-  console.log("Test");
+  contentContainer!.innerHTML = "";
   addHomeContent();
+});
+
+const menuButton = document.querySelector(".menuBtn");
+menuButton?.addEventListener("click", () => {
+  contentContainer!.innerHTML = "";
+  addMenuContent();
+});
+
+const contactButton = document.querySelector(".contactBtn");
+contactButton?.addEventListener("click", () => {
+  contentContainer!.innerHTML = "";
+  addContactContent();
 });
